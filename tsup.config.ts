26258d3a941424090d739b1391fbce95b662a8ca
vfasky <vfasky@me.com>
2021-12-29 17:31:09 +0800

    fix config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src/index.ts', 'tree-crawl'],
+    entry: ['src/index.ts'],
     splitting: false,
     sourcemap: true,
     minify: true,
